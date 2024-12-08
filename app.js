533f0d9a3bc2301c910e2dc6c5608b41da2e8515
sabbirRshuvo <sabbirhshuvo11@gmail.com>
2024-12-08 13:00:29 +0600

    almost done

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
-let mainBalance = 55000;
+let mainBalance = 5500;
 let balance1 = 0;
 let balance2 = 0;
 let balance3 = 0;
 const history = [];
+
+const firstLocation = document.getElementById("first-location").innerText;
+const secondLocation = document.getElementById("second-location").innerText;
+const thirdLocation = document.getElementById("third-location").innerText;
 
 function updateMainBalance() {
   document.getElementById("mainBalance").textContent = `
   $${mainBalance.toFixed(2)}
   `;
 }
-// to handle Donation
-
-function addToHistory(mainBalance, balance1, balance2, balance3) {
-  const historyItem = document.createElement("div");
-  historyItem.className =
-    "bg-white p-3 rounded-md border-1-2 border-indigo-500";
-
-  historyItem.innerHTML = `
-  <p class="text-xs text-gray-500">${new Date().toLocaleDateString()}</p>
-  
-  <p class="text-lg text-gray-500">Balance: ${mainBalance}</p>
-  <p class="text-xs text-gray-500">Donate:  B1: --${balance1} B2:-- ${balance2} B3:-- ${balance3}</p>
-
-  `;
-
-  const historyContainer = document.getElementById("history-list");
-  historyContainer.insertBefore(historyItem, historyContainer.firstChild);
-}
-//
 
 function handleHistory() {
   document.getElementById("history-btn").classList.add("hidden");
@@ -36,7 +21,7 @@ function handleHistory() {
     .classList.add("bg-green-500", "text-white");
   document.getElementById("buttonDonationPage").classList.add("bg-gray-400");
   document.getElementById("history-section").classList.remove("hidden");
-  addToHistory(mainBalance, balance1, balance2, balance3);
+  // addToHistory(mainBalance, balance1, balance2, balance3);
 }
 function handleDonate() {
   document.getElementById("history-btn").classList.remove("hidden");
@@ -47,7 +32,7 @@ function handleDonate() {
   document.getElementById("history-section").classList.add("hidden");
 }
 
-function handleDonation(inputId, balanceId, balanceVar) {
+function handleDonation(inputId, balanceId, balanceVar, location) {
   const donationAmount =
     parseFloat(document.getElementById(inputId).value) || 0;
 
@@ -58,6 +43,20 @@ function handleDonation(inputId, balanceId, balanceVar) {
     document.getElementById(balanceId).textContent = `$${balanceVar.toFixed(
       2
     )}`;
+
+    // history details
+    const historyItem = document.createElement("div");
+    historyItem.className = "bg-white rounded-md border-1-2 border-indigo-500";
+    historyItem.innerHTML = `
+        <h3>${donationAmount} Taka is Donated for Donate for ${location}</h3>
+        <p class="text-sm">
+        ${new Date().toLocaleString()}(BD Standard Time)
+        </p>
+     `;
+
+    const historyContainer = document.getElementById("history-list");
+    historyContainer.insertBefore(historyItem, historyContainer.firstChild);
+
     updateMainBalance();
     my_modal_1.showModal();
     // history
@@ -71,13 +70,13 @@ function handleDonation(inputId, balanceId, balanceVar) {
 }
 
 document.getElementById("donateButton1").addEventListener("click", function () {
-  balance1 = handleDonation("donation1", "balance1", balance1);
+  balance1 = handleDonation("donation1", "balance1", balance1, firstLocation);
 });
 document.getElementById("donateButton2").addEventListener("click", function () {
-  balance2 = handleDonation("donation2", "balance2", balance2);
+  balance2 = handleDonation("donation2", "balance2", balance2, secondLocation);
 });
 document.getElementById("donateButton3").addEventListener("click", function () {
-  balance3 = handleDonation("donation3", "balance3", balance3);
+  balance3 = handleDonation("donation3", "balance3", balance3, thirdLocation);
 });
 
 const navigateButton = document.getElementById("navigateButton");
